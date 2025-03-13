69995b699e0b2c2aa972f323898ad7ca82d53dc1
KPR23 <zabluka_aehit@students.vizja.pl>
2025-03-13 22:28:09 +0100

    Working fetching movie by ID

---
+++ src/app/api/movies/[id]/route.ts
@@ -0,0 +1,53 @@
+import { NextResponse } from 'next/server';
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.API_ACCESS_TOKEN}`,
+  },
+};
+
+interface Params {
+  id: string;
+}
+
+interface Props {
+  params: Params;
+}
+
+export async function GET(request: Request, { params }: Props) {
+  const { id } = await params;
+
+  if (!process.env.API_ACCESS_TOKEN) {
+    console.error('API_ACCESS_TOKEN is not defined in environment variables.');
+    return NextResponse.json(
+      { error: 'API key not configured' },
+      { status: 500 }
+    );
+  }
+
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}`,
+      options
+    );
+
+    if (!response.ok) {
+      console.error('HTTP error:', response.status);
+      return NextResponse.json(
+        { error: 'Failed to fetch data' },
+        { status: response.status }
+      );
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return NextResponse.json(
+      { error: 'Failed to fetch data' },
+      { status: 500 }
+    );
+  }
+}
